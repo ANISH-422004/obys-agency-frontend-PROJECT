82893b1a950da7621a9f3d8168e512591c8473c0
ANISH-422004 <anishbhattacharyagtl@gmail.com>
2024-09-27 17:15:35 +0530

    locomotive added on main div and its buggy idk why

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function locanimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
 function loderanimation(){
     gsap.from("#line-num",{
         opacity:0,
@@ -49,8 +84,8 @@ function loderanimation(){
     })
     tl.to("#loder",{
         opacity: 0,
-        duration: 0.5, // after work make it o.5
-        delay: 2.9,  // afeter work make it 2.9
+        duration: 0, // after work make it o.5
+        delay: 0,  // afeter work make it 2.9
         onComplete: function() {
                 document.getElementById("loder").style.display = 'none'; // Hide the loader completely
     }
@@ -67,7 +102,12 @@ function loderanimation(){
         stagger:0.2,
         lazy: false
     })
-    
+    tl.from("#hero-1 ,#page2",{
+        opacity:0,
+     
+    },"-=1.3") //"-=1.2" this helps to start this animation ahade of its time in the main timeline 
+
+
     tl.from("#nav",{
        opacity:0,
     })
@@ -77,8 +117,6 @@ function loderanimation(){
     
     
 }
-loderanimation()
-
 function cursorAnimation(){
 
     document.addEventListener("mousemove",function(dets){
@@ -93,6 +131,11 @@ function cursorAnimation(){
    Shery.makeMagnet("#nav-part2 h4", {});
    
 }
+
+
+loderanimation()
+
 cursorAnimation()
 
+locanimation()
 
